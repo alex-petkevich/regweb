5f5a9edb77b6a8fc8aaa3daa152277e8beb6b65b
mrdoggy <mrdoggy.all@gmail.com>
2015-02-17 18:10:08 +0300

    [*] bug fixes

---
+++ src/main/java/regweb/ConstPaths.java
@@ -0,0 +1,10 @@
+package regweb;
+
+/**
+ * @author alex on 2/17/15.
+ */
+public class ConstPaths {
+    public static final String ROOT_REDIRECT = "redirect:/";
+    public static final String FORMS = "forms";
+    public static final String ADD = "add";
+}
